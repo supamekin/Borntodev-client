f3de6f714b9442257c2d4c317e5b20cacc9e113d
supamekin <supamekin@gmail.com>
2023-11-05 23:57:22 +0700

    add

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
     element: <MainLayout />,
     children: [
       {
-        path: "home",
+        index: true,
         element: <HomePages />,
       },
       {
